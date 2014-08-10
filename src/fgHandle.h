2c4dd72124628b41940e6cc988787c4a169c1620
Radosław Gniazdowski <7131876+rgniazdowski@users.noreply.github.com>
2014-08-11 01:21:06 +0200

    (Part 16/X) First take on fixing Resource management. Added initial version for new Handle class and Handler Manager. In the end this should also be used for dereferencing via string ID. Spare some change?
    Signed-off-by: Vigilant <r.gniazdowski@gmail.com>

---
+++ src/fgHandle.h
@@ -0,0 +1,120 @@
+/*******************************************************
+ * Copyright (C) 2014 Radoslaw Gniazdowski <r.gniazdowski@gmail.com>. All rights reserved.
+ * 
+ * This file is part of #FLEXIGAME_PROJECT
+ * 
+ * #FLEXIGAME_PROJECT source code and any related files can not be copied, modified 
+ * and/or distributed without the express or written permission from the author.
+ *******************************************************/
+/**
+ * Portions Copyright (C) Scott Bilas, 2000
+ */
+
+#ifndef _FG_HANDLE_H_
+#define _FG_HANDLE_H_
+
+#include "fgCommon.h"
+
+#ifndef FG_RAW_HANDLE_TYPE
+#define FG_RAW_HANDLE_TYPE unsigned int
+#endif
+
+typedef FG_RAW_HANDLE_TYPE fgRawHandle;
+typedef FG_RAW_HANDLE_TYPE fgRawIndex;
+typedef FG_RAW_HANDLE_TYPE fgRawMagic;
+
+#ifndef FG_INVALID_HANDLE
+// All bits filled defines an invalid resource handle
+#define FG_INVALID_HANDLE			0xFFFFFFFFUL
+//#define FG_IS_INVALID_HANDLE(_rh)	((_rh.getHandle() == FG_INVALID_HANDLE) ? true : false)
+#define FG_IS_INVALID_HANDLE(_rh)	( _rh.isNull() )
+//#define FG_IS_VALID_HANDLE(_rh)		((_rh.getHandle() == FG_INVALID_HANDLE) ? false : true)
+#define FG_IS_VALID_HANDLE(_rh)	( !_rh.isNull() )
+#endif
+
+template <typename TagType>
+class fgHandle
+{
+private:
+    union
+    {
+        enum
+        {
+            // Size to use for index bit field
+            FG_HANDLE_MAX_BITS_INDEX = 16,
+            // Size to use for magic bit field
+            FG_HANDLE_MAX_BITS_MAGIC = 16,
+
+            // Size to compare against for asserting dereferences
+            FG_HANDLE_MAX_INDEX = ( 1 << FG_HANDLE_MAX_BITS_INDEX) - 1,
+            // Size to compare against for asserting dereferences
+            FG_HANDLE_MAX_MAGIC = ( 1 << FG_HANDLE_MAX_BITS_MAGIC) - 1,
+        };
+
+        struct
+        {
+			// index into resource array
+			fgRawIndex m_index : FG_HANDLE_MAX_BITS_INDEX;
+			// magic number to check
+            fgRawMagic m_magic : FG_HANDLE_MAX_BITS_MAGIC;
+        };
+        fgRawHandle m_handle;
+    };
+
+public:
+	// Default constructor for Handle object
+    fgHandle() : m_handle(FG_INVALID_HANDLE) { }
+
+    fgBool init(fgRawIndex index);
+
+    fgRawIndex getIndex(void) const {
+		return m_index;
+	}
+
+    fgRawMagic getMagic(void) const {
+		return m_magic;
+	}
+
+	fgRawHandle getHandle(void) const {
+		return m_handle;
+	}
+
+    fgBool isNull(void) const {
+		return (fgBool)(m_handle == FG_INVALID_HANDLE);
+	}
+
+	operator fgRawHandle(void) const {
+		return m_handle;
+	}
+};
+
+template <typename TagType>
+fgBool fgHandle<TagType>::init(fgRawIndex index)
+{
+	if(!isNull()) {
+		// Don't allow reassignment
+		return FG_FALSE;
+	}
+	if(index > FG_HANDLE_MAX_INDEX) {
+		// Verify range
+		return FG_FALSE;
+	}
+
+	static fgRawMagic s_autoMagic = 0;
+    if(++s_autoMagic >= FG_HANDLE_MAX_MAGIC) {
+        s_autoMagic = 1;
+    }
+
+    m_index = index;
+    m_magic = s_autoMagic;
+}
+
+template <typename TagType>
+inline bool operator != ( fgHandle <TagType> l, fgHandle <TagType> r )
+    {  return ( l.getHandle() != r.getHandle() );  }
+
+template <typename TagType>
+inline bool operator == ( fgHandle <TagType> l, fgHandle <TagType> r )
+    {  return ( l.getHandle() == r.getHandle() );  }
+
+#endif /* _FG_HANDLE_H_ */
---
+++ src/fgHandleManager.h
@@ -0,0 +1,192 @@
+/*******************************************************
+ * Copyright (C) 2014 Radoslaw Gniazdowski <r.gniazdowski@gmail.com>. All rights reserved.
+ * 
+ * This file is part of #FLEXIGAME_PROJECT
+ * 
+ * #FLEXIGAME_PROJECT source code and any related files can not be copied, modified 
+ * and/or distributed without the express or written permission from the author.
+ *******************************************************/
+/**
+ * Portions Copyright (C) Scott Bilas, 2000
+ */
+
+#ifndef _FG_HANDLE_MANAGER_H_
+#define _FG_HANDLE_MANAGER_H_
+
+#include "fgCommon.h"
+#include "fgHandle.h"
+
+#include <hash_map>
+
+template <typename DataType, typename HandleType>
+class fgHandleManager
+{
+public:
+    typedef fgArrayVector <DataType> hmDataVec;
+private:
+    typedef fgArrayVector <fgRawMagic>   hmMagicVec;
+    typedef fgArrayVector <unsigned int> hmFreeSlotsVec;
+	// Data storage
+    hmDataVec  m_managedData;
+	// Corresponding magic numbers
+    hmMagicVec m_magicData;
+	// Free slots in the database
+    hmFreeSlotsVec  m_FreeSlots;
+
+protected:
+	// Reset internal data
+	void clear(void);
+public:
+	// Default constructor for Handle Manager object
+	fgHandleManager() { clear(); }
+	// Default destructor for Handle Manager object
+	~fgHandleManager() { clear(); }
+
+	//
+    fgBool acquireHandle(HandleType& rHandle, DataType pResource);
+	//
+    fgBool  releaseHandle(HandleType handle);
+	//
+	void releaseAllHandles(void);
+
+	//
+    DataType dereference(HandleType handle);
+    //const DataType* Dereference( HandleType handle ) const;
+
+	//
+    unsigned int getUsedHandleCount(void) const {
+		return ( m_magicData.size() - m_FreeSlots.size());
+	}
+
+	//
+    bool hasUsedHandles(void) const {
+		return (!!GetUsedHandleCount());  
+	}
+
+	// 
+	hmDataVec& getRefDataVector(void) {
+		return m_managedData;
+	}
+
+	//
+	fgBool isDataManaged(DataType pData);
+	//
+	fgBool isHandleValid(HandleType handle);
+};
+
+/*
+ *
+ */
+template <typename DataType, typename HandleType>
+void fgHandleManager<DataType, HandleType>::clear(void)
+{
+	m_managedData.clear_optimised();
+	m_magicData.clear_optimised();
+	m_FreeSlots.clear_optimised();
+}
+
+/*
+ *
+ */
+template <typename DataType, typename HandleType>
+fgBool fgHandleManager<DataType, HandleType>::acquireHandle(HandleType& rHandle, DataType pResource)
+{
+    // If free list is empty, add a new one otherwise use first one found
+    unsigned int index;
+    if(m_FreeSlots.empty()) {
+        index = m_magicData.size();
+        if(!rHandle.init(index))
+			return FG_FALSE;
+        m_managedData.push_back(pResource);
+        m_magicData.push_back(rHandle.getMagic());
+    } else {
+        index = m_FreeSlots.back();
+		if(!rHandle.init(index))
+			return FG_FALSE;
+        m_FreeSlots.pop_back();
+		m_managedData[index] = pResource;
+        m_magicData[index] = rHandle.getMagic();
+    }
+    return FG_TRUE;
+}
+
+/*
+ *
+ */
+template <typename DataType, typename HandleType>
+fgBool fgHandleManager<DataType, HandleType>::releaseHandle(HandleType handle)
+{
+	if(!isHandleValid(handle))
+		return FG_FALSE;
+    // which one?
+    fgRawIndex index = handle.getIndex();
+    // ok remove it - tag as unused and add to free list
+    m_magicData[index] = 0;
+	m_managedData[index] = NULL;
+    m_FreeSlots.push_back(index);
+	return FG_TRUE;
+}
+
+/*
+ *
+ */
+template <typename DataType, typename HandleType>
+void fgHandleManager<DataType, HandleType>::releaseAllHandles(void)
+{
+	clear();
+}
+
+/*
+ *
+ */
+template <typename DataType, typename HandleType>
+inline DataType fgHandleManager<DataType, HandleType>::dereference(HandleType handle)
+{
+	if(!isHandleValid(handle))
+		return NULL;
+	fgRawIndex index = handle.getIndex();
+    return *(m_managedData.begin() + index);
+}
+
+/*
+ *
+ */
+template <typename DataType, typename HandleType>
+inline fgBool fgHandleManager<DataType, HandleType>::isDataManaged(DataType pData)
+{
+	if(m_managedData.find(pData) != -1)
+		return FG_TRUE;
+	return FG_FALSE;
+}
+
+/*
+ *
+ */
+template <typename DataType, typename HandleType>
+inline fgBool fgHandleManager<DataType, HandleType>::isHandleValid(HandleType handle)
+{
+	if(handle.isNull())
+		return FG_FALSE;
+	// check handle validity - $ this check can be removed for speed
+    // if you can assume all handle references are always valid.
+	fgRawIndex index = handle.getIndex();
+    if ((index >= m_managedData.size()) || (m_magicData[index] != handle.getMagic()))
+    {
+        // no good! invalid handle == client programming error
+        return FG_FALSE;
+    }
+	return FG_TRUE;
+}
+
+#if 0
+template <typename DataType, typename HandleType>
+inline const DataType* fgHandleManager <DataType, HandleType>
+:: Dereference( HandleType handle ) const
+{
+    // this lazy cast is ok - non-const version does not modify anything
+    typedef fgHandleManager <DataType, HandleType> ThisType;
+    return ( const_cast <ThisType*> ( this )->Dereference( handle ) );
+}
+#endif
+
+#endif /* _FG_HANDLE_MANAGER_H_ */
