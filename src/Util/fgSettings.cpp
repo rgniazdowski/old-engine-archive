490bde7a396112afac5eb660a28420d3fe622151
Radosław Gniazdowski <7131876+rgniazdowski@users.noreply.github.com>
2014-08-26 02:58:00 +0200

    Added (almost finished) version of main settings parser (xml) - uses xml auto parser (template and macro based definitions). That is fgSettings class. Initial test done, works... moving on.
    -- The spice extends life...

--- src/Util/fgSettings.cpp
+++ src/Util/fgSettings.cpp
@@ -6,3 +6,66 @@
  * #FLEXIGAME_PROJECT source code and any related files can not be copied, modified 
  * and/or distributed without the express or written permission from the author.
  *******************************************************/
+
+#include "fgSettings.h"
+
+/*
+ *
+ */
+fgSettings::fgSettings() : m_parser(NULL) {
+
+}
+
+/*
+ *
+ */
+fgSettings::fgSettings(const char *filePath) : m_parser(NULL)
+{
+	load(filePath);
+}
+
+/*
+ *
+ */
+fgSettings::~fgSettings()
+{
+}
+
+fgBool fgSettings::load(const char *filePath)
+{
+	if(!filePath)
+		return FG_FALSE;
+	if(!m_parser) {
+		m_parser = new fgXMLParser();
+		m_parser->setReportToMsgSystem(FG_FALSE);
+	}
+	fgBool status = FG_TRUE;
+	fgXMLAutoHandler<fgSettings::settingsData> *content = NULL;
+	content = new fgXMLAutoHandler<fgSettings::settingsData>();
+	settingsData data;
+	content->setTarget(&data);
+	m_parser->setContentHandler(content);
+	if(!m_parser->loadXML(filePath)) {
+		status = FG_FALSE;
+	} else {
+		if(!m_parser->parseWithHandler()) {
+			status = FG_FALSE;
+		}
+	}
+	delete content;
+	content = NULL;
+	m_parser->freeXML();
+
+	if(status) {
+
+		printf("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+		printf(">>>>>>> defaultDataPath: '%s'\n", data.defaultDataPath);
+		printf(">>>>>>> defaultLogPath:  '%s'\n", data.defaultLogPath);
+		printf(">>>>>>> programTitle:    '%s'\n", data.programTitle);
+		printf(">>>>>>> useConsole:      '%d'\n", data.useConsole);
+		printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n");
+	}
+	delete m_parser;
+	m_parser = NULL;
+	return status;
+}
--- src/Util/fgSettings.h
+++ src/Util/fgSettings.h
@@ -7,7 +7,92 @@
  * and/or distributed without the express or written permission from the author.
  *******************************************************/
 
-#ifndef _FG_SETTINGS_H
-#define _FG_SETTINGS_H
+#ifndef _FG_SETTINGS_H_
+#define _FG_SETTINGS_H_
 
-#endif /* _FG_SETTINGS_H */
+#include "fgTypes.h"
+#include "XML/fgXMLParser.h"
+#include "XML/fgXMLDefaultHandler.h"
+#include "XML/fgXMLAutoHandler.h"
+
+
+/*
+ *
+ */
+class fgSettings {
+public:
+	struct settingsData {
+		std::string defaultDataPath;
+		std::string defaultLogPath;
+		std::string mainConfigPath;
+		std::string programTitle;
+		std::string defaultProfileName;
+		std::string mainProfileName;
+		std::string lastExecution;
+		int	videoModeID;
+		int verboseLevel;
+		long lastTimestamp;
+		fgBool useSound;
+		fgBool useConsole;
+		fgBool useNetwork;
+		fgBool cleanExit;
+		fgBool debugMode;
+
+		settingsData() :
+			videoModeID(-1),
+			verboseLevel(0),
+			lastTimestamp(0L),
+			useSound(FG_FALSE),
+			useConsole(FG_FALSE),
+			useNetwork(FG_FALSE),
+			cleanExit(FG_FALSE),
+			debugMode(FG_FALSE)
+		{
+		}
+
+		void clear(void) {
+
+		}
+	};
+public:
+	fgSettings();
+	fgSettings(const char *filePath);
+	~fgSettings();
+
+	fgBool load(const char *filePath);
+protected:
+	fgXMLParser *m_parser;
+	settingsData m_settings;
+};
+
+#define FG_SETTINGS_XML_ROOT_NODE_NAME	"Settings"
+
+FG_XML_AUTO_TEMPLATE_ROOT(fgSettings::settingsData, FG_SETTINGS_XML_ROOT_NODE_NAME);
+
+FG_XML_AUTO_TEMPLATE_ELEMENT_BEGIN(fgSettings::settingsData);
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(const char *, defaultDataPath,		"defaultDataPath");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(const char *, defaultLogPath,		"defaultLogPath");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(const char *, mainConfigPath,		"mainConfigPath");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(const char *, programTitle,			"programTitle");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(const char *, defaultProfileName,	"defaultProfileName");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(const char *, mainProfileName,		"mainProfileName");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(const char *, lastExecution,		"lastExecution");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(int,		videoModeID,		"videoModeID");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(int,		verboseLevel,		"verboseLevel");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(long,		lastTimestamp,		"lastTimestamp");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(fgBool,		useSound,			"useSound");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(fgBool,		useConsole,			"useConsole");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(fgBool,		useNetwork,			"useNetwork");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(fgBool,		cleanExit,			"cleanExit");
+FG_XML_AUTO_TEMPLATE_ELEMENT_CHECK_NAME(fgBool,		debugMode,			"debugMode");
+FG_XML_AUTO_TEMPLATE_ELEMENT_END();
+
+FG_XML_AUTO_TEMPLATE_CHARACTERS_BEGIN(fgSettings::settingsData);
+FG_XML_AUTO_TEMPLATE_CHARACTERS_EMPTY
+FG_XML_AUTO_TEMPLATE_CHARACTERS_END();
+
+FG_XML_AUTO_TEMPLATE_ATTRIBUTE_BEGIN(fgSettings::settingsData);
+FG_XML_AUTO_TEMPLATE_ATTRIBUTE_EMPTY
+FG_XML_AUTO_TEMPLATE_ATTRIBUTE_END();
+
+#endif /* _FG_SETTINGS_H_ */
