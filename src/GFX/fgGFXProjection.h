eb2558839a30011e596fccd9ad1a062147bd907c
Radosław Gniazdowski <7131876+rgniazdowski@users.noreply.github.com>
2014-09-17 13:21:49 +0200

    [GFX] Added Projection class for managing the Projection matrix - contains functions for setting perspective or ortho projection

--- src/GFX/fgGFXProjection.h
+++ src/GFX/fgGFXProjection.h
@@ -11,15 +11,13 @@
 #define _FG_GFX_PROJECTION_H_
 
 #include "fgBuildConfig.h"
-#include "glm/mat4x4.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include <glm/gtc/type_ptr.hpp>
+#include "Math/fgMathLib.h"
 
 #define FG_GFX_PERSPECTIVE_ZNEAR_DEFAULT	0.01f
 #define FG_GFX_PERSPECTIVE_ZFAR_DEFAULT		1024.0f
 
-#define FG_GFX_PERSPECTIVE_FOVY_DEFAULT		90.0f
-#define FG_GFX_PERSPECTIVE_FOV_DEFAULT		90.0f
+#define FG_GFX_PERSPECTIVE_FOVY_DEFAULT		45.0f
+#define FG_GFX_PERSPECTIVE_FOV_DEFAULT		45.0f
 
 #define FG_GFX_PERSPECTIVE_ASPECT_DEFAULT	(1024.0f/768.0f)
 
@@ -28,11 +26,13 @@
 /*
  *
  */
-class fgGfxProjection {
+class fgGfxProjection 
+{
 protected:
 	///
-	glm::mat4x4 m_matrix;
+	fgMatrix4f m_projMatrix;
 private:
+
 public:
 	//
 	fgGfxProjection() {}
@@ -41,27 +41,33 @@ public:
 
 	//
 	inline void setPerspective(float fovY, float aspect, float zNear = FG_GFX_PERSPECTIVE_ZNEAR_DEFAULT, float zFar = FG_GFX_PERSPECTIVE_ZFAR_DEFAULT) {
-		m_matrix = glm::perspective(fovY, aspect, zNear, zFar);
+		float properFOV;
+#if defined GLM_FORCE_RADIANS
+		properFOV = glm::radians(fovY);
+#else
+		properFOV = fovY;
+#endif
+		m_projMatrix = glm::perspective(properFOV, aspect, zNear, zFar);
 	}
 
 	//
 	inline void setOrtho(float left, float right, float bottom, float top, float zNear = FG_GFX_ORTHO_ZNEAR_DEFAULT, float zFar = FG_GFX_ORTHO_ZFAR_DEFAULT) {
-		m_matrix = glm::ortho(left, right, bottom, top, zNear, zFar);
+		m_projMatrix = glm::ortho(left, right, bottom, top, zNear, zFar);
 	}
 
 	//
-	glm::mat4x4 & getRefMatrix(void) {
-		return m_matrix;
+	fgMatrix4f & getRefProjMatrix(void) {
+		return m_projMatrix;
 	}
 
 	// 
-	const float * getPointer(void) const {
-		return glm::value_ptr(m_matrix);
+	const float * getProjMatPtr(void) const {
+		return glm::value_ptr(m_projMatrix);
 	}
 
 	// 
-	float * getPointer(void) {
-		return glm::value_ptr(m_matrix);
+	float * getProjMatPtr(void) {
+		return glm::value_ptr(m_projMatrix);
 	}
 };
 
