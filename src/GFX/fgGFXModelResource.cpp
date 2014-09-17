e449a165ed6e0e1c612cc85925f36bffae58a9ee
Radosław Gniazdowski <7131876+rgniazdowski@users.noreply.github.com>
2014-09-17 13:40:32 +0200

    [GFX] Model resource class - now loads the .obj file using tinyobjloader - still needs fixing, but works for now. ! It works tremendously slow, I mean... seriously - need to change the tinyobj code

--- src/GFX/fgGFXModelResource.cpp
+++ src/GFX/fgGFXModelResource.cpp
@@ -8,3 +8,238 @@
  *******************************************************/
 
 #include "fgGFXModelResource.h"
+#include "tiny_obj_loader.h"
+#include "Resource/fgResourceErrorCodes.h"
+
+/*
+ *
+ */
+fgGfxModelResource::fgGfxModelResource() :
+	m_materialOverride(NULL),
+	m_modelType(FG_GFX_MODEL_RES_INVALID),
+	m_isMultitextured(FG_FALSE),
+	m_isTextured(FG_FALSE),
+	m_hasMaterial(FG_FALSE),
+	m_isInterleaved(FG_TRUE)
+{
+	memset(m_numData, 0, sizeof(m_numData));
+	m_resType = FG_RESOURCE_TEXTURE;
+}
+
+/*
+ *
+ */
+fgGfxModelResource::fgGfxModelResource(const char *path) :
+	m_materialOverride(NULL),
+	m_modelType(FG_GFX_MODEL_RES_INVALID),
+	m_isMultitextured(FG_FALSE),
+	m_isTextured(FG_FALSE),
+	m_hasMaterial(FG_FALSE),
+	m_isInterleaved(FG_TRUE),
+	fgResource(path)
+{
+	m_resType = FG_RESOURCE_TEXTURE;
+}
+
+/*
+ *
+ */
+fgGfxModelResource::fgGfxModelResource(std::string& path) : 
+	m_materialOverride(NULL),
+	m_modelType(FG_GFX_MODEL_RES_INVALID),
+	m_isMultitextured(FG_FALSE),
+	m_isTextured(FG_FALSE),
+	m_hasMaterial(FG_FALSE),
+	m_isInterleaved(FG_TRUE),
+	fgResource(path)
+{
+	m_resType = FG_RESOURCE_TEXTURE;
+}
+
+/*
+ * Clears the class data, this actually does not free allocated memory,
+ * just resets base class attributes
+ */
+void fgGfxModelResource::clear(void)
+{
+	fgResource::clear();
+	m_materialOverride = NULL;
+	m_modelType = FG_GFX_MODEL_RES_INVALID;
+	m_isTextured = FG_FALSE;
+	m_isMultitextured = FG_FALSE;
+	m_hasMaterial = FG_FALSE;
+	m_isInterleaved = FG_TRUE;
+	memset(m_numData, 0, sizeof(m_numData));
+}
+
+// #FIXME #PLOX
+fgBool fgGfxModelResource::_loadOBJ(void)
+{
+	if(getFilePath(m_quality).empty()) {
+		return FG_FALSE;
+	}
+	// #FIXME - should not use tinyobj types here
+	std::vector<tinyobj::shape_t> shapes;
+	std::string mtl_basepath = fgPath::dirName(getFilePath());
+	std::string err = tinyobj::LoadObj(shapes, getFilePathStr(m_quality), mtl_basepath.c_str());
+
+	if(!err.empty()) {
+		FG_LOG::PrintError("Error while loading model: %s", err.c_str());
+		return FG_FALSE;
+	}
+	memset(m_numData, 0, sizeof(m_numData));
+	m_isReady = FG_FALSE;
+	m_size = 0;
+	std::vector<tinyobj::shape_t>::iterator begin, end, itor;
+	begin=shapes.begin(); end=shapes.end(); itor=begin;
+	if(shapes.empty()) {
+		FG_LOG::PrintError("There is no shapes in model file: '%s'", getFilePathStr());
+	}
+	int n = 0;
+	for(;itor!=end;itor++) {
+		m_numShapes++;
+		tinyobj::shape_t & tinyshape = (*itor);
+		m_shapes.push_back();
+		n = m_shapes.size()-1;
+		FG_LOG::PrintDebug("Model '%s': found shape '%s'", getNameStr(), tinyshape.name.c_str());
+		m_shapes[n].name = tinyshape.name;
+		m_shapes[n].material = new fgGfxMaterial(tinyshape.material);
+		m_numMaterials++;
+		if(m_isInterleaved)
+			m_shapes[n].mesh = new fgGfxMeshAoS(tinyshape.mesh);
+		else
+			m_shapes[n].mesh = new fgGfxMeshSoA(tinyshape.mesh);
+		m_numVertices += m_shapes[n].mesh->getNumVertices();
+		m_numNormals += m_shapes[n].mesh->getNumNormals();
+		m_numUVs += m_shapes[n].mesh->getNumUVs();
+		m_numIndices += m_shapes[n].mesh->getNumIndices();
+		tinyshape.mesh.indices.clear();
+		tinyshape.mesh.normals.clear();
+		tinyshape.mesh.positions.clear();
+		tinyshape.mesh.texcoords.clear();
+		tinyshape.name.clear();
+		tinyshape.material.unknown_parameter.clear();
+		m_size += m_shapes[n].getSize();
+	}
+	shapes.clear();
+	if(!m_materialOverride)
+		m_materialOverride = new fgGfxMaterial();
+	m_size += m_materialOverride->getSize();
+	FG_LOG::PrintDebug("Loaded OBJ file: total size in bytes: %d", m_size);
+	FG_LOG::PrintDebug("Model '%s': vertices: %d, indices: %d, uvs: %d", getNameStr(), 
+		m_numVertices, m_numIndices, m_numUVs);
+	return FG_TRUE;
+}
+
+/*
+ * Create function loads/interprets data from file in ROM and place it in RAM memory.
+ */
+fgBool fgGfxModelResource::create(void)
+{
+	if(m_isReady || !isDisposed()) {
+		return FG_TRUE;
+	}
+	clearStatus();
+	m_size = 0;
+	if(getFilePath(m_quality).empty()) {
+		FG_LOG::PrintError("%s(%d): file path is empty on create", fgPath::fileName(__FILE__), __LINE__-1); 
+		// #TODO error handling / reporting
+		return FG_FALSE;
+	}
+	setModelTypeFromFilePath();
+	if(m_modelType == FG_GFX_MODEL_RES_INVALID) {
+		FG_LOG::PrintError("%s(%d): model file type is invalid", fgPath::fileName(__FILE__), __LINE__-1); 
+		// #TODO error handling / reporting
+		return FG_FALSE;
+	}
+	switch(m_modelType)
+	{
+	case FG_GFX_MODEL_RES_CUSTOM:
+		break;
+
+	case FG_GFX_MODEL_RES_3DS:
+		break;
+		
+	case FG_GFX_MODEL_RES_OBJ:
+		FG_LOG::PrintDebug("Preparing to load an OBJ file for model: '%s'", getNameStr());
+		if(!_loadOBJ()) {
+			return FG_FALSE;
+		}
+		m_isReady = FG_TRUE;
+		reportSuccess(FG_ERRNO_RESOURCE_OK, "Successfully loaded an OBJ model file: '%s'", getFilePathStr());
+		break;
+
+	case FG_GFX_MODEL_RES_BLEND:
+		break;
+
+	case FG_GFX_MODEL_RES_DAE:
+		break;
+
+	case FG_GFX_MODEL_RES_DXF:
+		break;
+
+	case FG_GFX_MODEL_RES_FBX:
+		break;
+
+	case FG_GFX_MODEL_RES_LWO:
+		break;
+
+	case FG_GFX_MODEL_RES_OFF:
+		break;
+
+	case FG_GFX_MODEL_RES_X:
+		break;
+
+	default:
+		return FG_FALSE;
+		break;
+	};
+
+	if(!m_size) return FG_FALSE;
+
+	return FG_TRUE;
+}
+
+/*
+ * Destroy all loaded data including additional metadata (called with deconstructor)
+ */
+void fgGfxModelResource::destroy(void)
+{
+	dispose();
+	clear();
+}
+
+/*
+ * Reloads any data, recreates the resource (refresh)
+ */
+fgBool fgGfxModelResource::recreate(void)
+{
+	dispose();
+	return create();
+}
+
+/*
+ * Dispose completely of the all loaded data, free all memory
+ */
+void fgGfxModelResource::dispose(void)
+{
+	int n = (int)m_shapes.size();
+	for(int i=0;i<n;i++) {
+		m_shapes[i].clear();
+	}
+	m_shapes.clear_optimised();
+	if(m_materialOverride) {
+		delete m_materialOverride;
+	}
+	m_materialOverride = NULL;
+	m_hasMaterial = FG_FALSE;
+	m_isReady = FG_FALSE;
+}
+
+/*
+ * Check if resource is disposed (not loaded yet or disposed after)
+ */
+fgBool fgGfxModelResource::isDisposed(void) const
+{
+	return (fgBool)(m_shapes.empty());
+}
--- src/GFX/fgGFXModelResource.h
+++ src/GFX/fgGFXModelResource.h
@@ -10,97 +10,201 @@
 #ifndef _FG_GFX_MODEL_RESOURCE_H_
 #define _FG_GFX_MODEL_RESOURCE_H_
 
-#include <string>
-#include <map>
-
-#include "fgGFXTypes.h"
-#include "fgCommon.h"
+#include "fgTypes.h"
+#include "fgGFXModelTypes.h"
 #include "Resource/fgResource.h"
-#include "Resource/fgResourceManager.h"
-#include "Math/fgMathLib.h"
 
-#include "fgGFXMaterial.h"
-
-struct fgGfxMesh
-{
-	fgArrayVector<float>          vertices;
-	fgArrayVector<float>          normals;
-	fgArrayVector<float>          uvs;
-	fgArrayVector<unsigned int>   indices;
-
-	void clear(void)
-	{
-		vertices.clear_optimised();
-		normals.clear_optimised();
-		uvs.clear_optimised();
-		indices.clear_optimised();
-	}
-
-};
-
-struct fgGfxShape
-{
-	std::string		name;
-	fgGfxMaterial   material;
-	fgGfxMesh		mesh;
-
-	void clear(void)
-	{
-		mesh.clear();
-		material.clear();
-	}
-};
+// invalid / not selected file format
+#define FG_GFX_MODEL_RES_INVALID	0x000
+// Custom 3d model file format
+#define	FG_GFX_MODEL_RES_CUSTOM		0x0F0
+// 3DS - Autodesk 3ds Max 3D
+#define	FG_GFX_MODEL_RES_3DS		0x0F1
+// OBJ - Wavefront Technologies
+#define	FG_GFX_MODEL_RES_OBJ		0x0F2
+// BLEND - Blender file database
+#define	FG_GFX_MODEL_RES_BLEND		0x0F3
+// DAE - COLLADA
+#define	FG_GFX_MODEL_RES_DAE		0x0F4
+// DXF - AutoCAD, Drawing Exchange Format - CAD data file format developed by Autodesk
+#define	FG_GFX_MODEL_RES_DXF		0x0F5
+// FBX - Autodesk exchange - exchange format, in particular for interoperability between Autodesk products and other Digital content creation (DCC) software packages
+#define	FG_GFX_MODEL_RES_FBX		0x0F6
+// LWO - Lightwave - LightWave high-end software file format
+#define	FG_GFX_MODEL_RES_LWO		0x0F7
+// OFF - Object File Format 
+#define	FG_GFX_MODEL_RES_OFF		0x0F8
+// X - DirectX 3D Model
+#define	FG_GFX_MODEL_RES_X			0x0F9
 
 class fgGfxModelResource : public fgResource
 {
 public:
-	enum fgGfxModelResType {
-		// Custom 3d model file format
-		FG_GFX_MODEL_RES_CUSTOM,
-		// 3DS - Autodesk 3ds Max 3D
-		FG_GFX_MODEL_RES_3DS, 
-		// OBJ - Wavefront Technologies
-		FG_GFX_MODEL_RES_OBJ,
-		// BLEND - Blender file database
-		FG_GFX_MODEL_RES_BLEND,
-		// DAE - COLLADA
-		FG_GFX_MODEL_RES_DAE,
-		// DXF - AutoCAD, Drawing Exchange Format - CAD data file format developed by Autodesk
-		FG_GFX_MODEL_RES_DXF,
-		// FBX - Autodesk exchange - exchange format, in particular for interoperability between Autodesk products and other Digital content creation (DCC) software packages
-		FG_GFX_MODEL_RES_FBX,
-		// LWO - Lightwave - LightWave high-end software file format
-		FG_GFX_MODEL_RES_LWO,
-		// OFF - Object File Format 
-		FG_GFX_MODEL_RES_OFF,
-		// X - DirectX 3D Model
-		FG_GFX_MODEL_RES_X
-	};
+	typedef fgVector<fgGfxShape>	modelShapes;
+	typedef modelShapes::iterator	modelShapesItor;
+
+public:
+	//
+	fgGfxModelResource();
+	//
+	fgGfxModelResource(const char *path);
+	//
+	fgGfxModelResource(std::string& path);
+	//
+	virtual ~fgGfxModelResource()	{ destroy(); }
+
+	FG_RESOURCE_FACTORY_CREATE_FUNCTION(fgGfxModelResource)
 protected:
-	fgGfxModelResType m_modelResourceType;
-	std::string m_modelName;
-	fgBool	m_isMultitextured;
-	fgBool	m_isTextured;
-	fgBool	m_hasMaterial;
+	// Clears the class data, this actually does not free allocated memory,
+	// just resets base class attributes
+	virtual void clear(void);
+public:
+	// Create function loads/interprets data from file in ROM and place it in RAM memory.
+	virtual fgBool create(void);
+	// Destroy all loaded data including additional metadata (called with deconstructor)
+	virtual void destroy(void);
+	// Reloads any data, recreates the resource (refresh)
+	virtual fgBool recreate(void);
+	// Dispose completely of the all loaded data, free all memory
+	virtual void dispose(void);
+	// Check if resource is disposed (not loaded yet or disposed after)
+	virtual fgBool isDisposed(void) const;
+
+protected:
+	// 
+	fgBool setModelTypeFromFilePath(std::string &path) {
+		const char *ext = fgPath::fileExt(path.c_str());
+		if(!ext)
+			return FG_FALSE;
+		m_modelType = FG_GFX_MODEL_RES_INVALID;
+		if(strcasecmp(ext, FG_GFX_MODEL_RES_3DS_EXTENSION) == 0) {
+			m_modelType = FG_GFX_MODEL_RES_3DS;
+		} else if(strcasecmp(ext, FG_GFX_MODEL_RES_OBJ_EXTENSION) == 0) {
+			m_modelType = FG_GFX_MODEL_RES_OBJ;
+		}
+		return FG_TRUE;
+	}
+
+	// 
+	fgBool setModelTypeFromFilePath(void) {
+		if(getFilePath(m_quality).empty())
+			return FG_FALSE;
+		return setModelTypeFromFilePath(getFilePath(m_quality));
+	}
+
+	fgBool _loadOBJ(void);
+
+public:
+	//
+	modelShapes	&	getRefShapes(void) {
+		return m_shapes;
+	}
+
+	//
+	fgGfxMaterial* getMainMaterial(void) const {
+		return m_materialOverride;
+	}
+
+	//
+	fgGFXuint	getModelType(void) const {
+		return m_modelType;
+	}
+
+	//
+	fgBool	isMultitextured(void) const {
+		return m_isMultitextured;
+	}
+
+	//
+	fgBool isTextured(void) const {
+		return m_isTextured;
+	}
+
+	//
+	fgBool hasMaterial(void) const {
+		return m_hasMaterial;
+	}
+
+	//
+	fgBool isInterleaved(void) const {
+		return m_isInterleaved;
+	}
+
+	//
+	void setInterleaved(fgBool toggle = FG_TRUE) {
+		m_isInterleaved = toggle;
+	}
+
+	//
+	fgGFXuint getNumVertices(void) const {
+		return m_numVertices;
+	}
+	//
+	fgGFXuint getNumNormals(void) const {
+		return m_numNormals;
+	}
+	//
+	fgGFXuint getNumUVs(void) const {
+		return m_numUVs;
+	}
+	//
+	fgGFXuint getNumIndices(void) const {
+		return m_numIndices;
+	}
+	//
+	fgGFXuint getNumTriangles(void) const {
+		return m_numTriangles;
+	}
+	//
+	fgGFXuint getNumPolygons(void) const {
+		return m_numPolygons;
+	}
+	//
+	fgGFXuint getNumShapes(void) const {
+		return m_numShapes;
+	}
+	//
+	fgGFXuint getNumMaterials(void) const {
+		return m_numMaterials;
+	}
+	//
+	const fgGFXuint* getNumData(void) const {
+		return m_numData;
+	}
+
+protected:
+	/// 
+	fgGfxMaterial	*m_materialOverride;
+	/// 
+	modelShapes		m_shapes;
+	/// 
+	fgGFXuint		m_modelType;
+	/// 
+	fgBool			m_isMultitextured;
+	/// 
+	fgBool			m_isTextured;
+	/// 
+	fgBool			m_hasMaterial;
+	/// 
+	fgBool			m_isInterleaved;
 
 	union {
 		struct {
 			union {
-				unsigned int m_numVertices;
-				unsigned int m_numPoints;
+				fgGFXuint m_numVertices;
+				fgGFXuint m_numPoints;
 			};
-			unsigned int m_numNormals;		
-			unsigned int m_numUVs;
-			unsigned int m_numIndices;
-			unsigned int m_numTriangles;
-			unsigned int m_numPolygons;
-			unsigned int m_numShapes;
-			unsigned int m_numMaterials;
+			fgGFXuint m_numNormals;		
+			fgGFXuint m_numUVs;
+			fgGFXuint m_numIndices;
+			fgGFXuint m_numTriangles;
+			fgGFXuint m_numPolygons;
+			fgGFXuint m_numShapes;
+			fgGFXuint m_numMaterials;
 		};
-		unsigned int m_numData[8];
+		fgGFXuint m_numData[8];
 	};
-	fgGfxMaterial m_materialOverride;
-	fgArrayVector<fgGfxShape> m_shapes;
+	
 };
 
 #endif /* _FG_GFX_MODEL_RESOURCE_H_ */
