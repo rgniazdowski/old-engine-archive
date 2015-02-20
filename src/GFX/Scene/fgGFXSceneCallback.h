8d783117af09e13f63d1d357367842e81262bd31
Radosław Gniazdowski <7131876+rgniazdowski@users.noreply.github.com>
2015-02-20 17:16:12 +0100

    Scene: Added initial version of classes and structures for Scene event management: event::Scene structs, CSceneCallback standard object

---
+++ src/GFX/Scene/fgGFXSceneCallback.h
@@ -0,0 +1,363 @@
+/*******************************************************
+ * Copyright (C) 2014 Radoslaw Gniazdowski <contact@flexigame.com>. All rights reserved.
+ * 
+ * This file is part of FlexiGame: Flexible Game Engine
+ * 
+ * FlexiGame source code and any related files can not be copied, modified 
+ * and/or distributed without the express or written consent from the author.
+ *******************************************************/
+/* 
+ * File:   fgGFXSceneCallback.h
+ * Author: vigilant
+ *
+ * Created on February 18, 2015, 11:04 PM
+ */
+
+#ifndef FG_INC_GFX_SCENE_CALLBACK
+    #define FG_INC_GFX_SCENE_CALLBACK
+    #define FG_INC_GFX_SCENE_CALLBACK_BLOCK
+
+    #include "Event/fgCallback.h"
+
+namespace fg {
+    namespace gfx {
+        
+        class CSceneNode;
+        class CSceneNodeTrigger;
+        class CSceneManager;
+
+        /**
+         *
+         */
+        class CSceneCallback : public virtual fg::event::CFunctionCallback {
+            friend class CSceneNode;
+            friend class CSceneNodeTrigger;
+            friend class CSceneManager;
+            
+        public:
+            ///
+            typedef fg::event::CFunctionCallback base_type;
+            ///
+            typedef CSceneCallback self_type;
+            ///
+            typedef CSceneCallback type;
+
+        public:
+            /**
+             * 
+             * @param pGuiMain
+             */
+            CSceneCallback() : m_ref(0) {
+                fg::event::CFunctionCallback::setFunction((fg::event::CFunctionCallback::fgFunction)NULL);
+            }
+            /**
+             * 
+             */
+            virtual ~CSceneCallback() { }
+
+            using base_type::Call;
+            
+            /**
+             * 
+             * @param pNodeA
+             * @return 
+             */
+            virtual fgBool Call(CSceneNode* pNodeA) = 0;
+            
+            /**
+             * 
+             * @param pNodeA
+             * @param pNodeB
+             * @return 
+             */
+            virtual fgBool Call(CSceneNode* pNodeA, CSceneNode* pNodeB) = 0;
+            
+        private:
+            ///
+            inline void upRef(void) { m_ref++; }
+            ///
+            inline unsigned int downRef(void) { if(m_ref) m_ref--; return m_ref; }            
+            
+        private:
+            ///
+            unsigned int m_ref;            
+        };
+
+        /**
+         *
+         */
+        template < class TClass >
+        class CSceneMethodCallback : public CSceneCallback {
+        public:
+            ///
+            typedef CSceneCallback base_type;
+            ///
+            typedef CSceneMethodCallback self_type;
+            ///
+            typedef CSceneMethodCallback type;
+            ///
+            typedef fgBool(TClass::*fgSceneMethod)(CSceneNode* pNodeA, CSceneNode* pNodeB);
+            ///
+            typedef fgSceneMethod callback_type;
+            
+        public:
+            /**
+             * 
+             * @param pClassInstance
+             * @param pMethod
+             */
+            CSceneMethodCallback(TClass* pClassInstance, fgSceneMethod pMethod) :
+            CSceneCallback() {
+                m_pClassInstance = pClassInstance;
+                m_sceneMethod = pMethod;
+            }
+            /**
+             * 
+             */
+            virtual ~CSceneMethodCallback() {
+                m_pClassInstance = NULL;
+                m_sceneMethod = NULL;
+            }
+            /**
+             * 
+             * @param pClassInstance
+             */
+            void setClassInstance(TClass* pClassInstance) {
+                if(pClassInstance)
+                    m_pClassInstance = pClassInstance;
+            }
+            /**
+             * 
+             * @param method
+             */
+            void setSceneMethod(fgSceneMethod method) {
+                m_pClassInstance = NULL;
+                m_sceneMethod = method;
+            }
+            /**
+             * 
+             * @return 
+             */
+            virtual fgBool Call(void) {
+                if(m_sceneMethod != NULL && m_pClassInstance != NULL)
+                    return (m_pClassInstance->*m_sceneMethod)(NULL, NULL);
+                return FG_FALSE;
+            }
+            /**
+             * 
+             * @param argv
+             * @return 
+             */
+            virtual fgBool Call(fg::event::CArgumentList* argv) {
+                if(m_sceneMethod != NULL && m_pClassInstance != NULL) {
+                    // If argument list is given then maybe should check for existence
+                    // of STRUCT parameters ?
+                    //return (m_classInstance->*m_guiMethod)(argv);
+                    return FG_FALSE;
+                }
+                return FG_FALSE;
+            }
+            /**
+             * 
+             * @param pSystemData
+             * @return 
+             */
+            virtual fgBool Call(void *pSystemData) {
+                // This is for compatibility - fgFunctionCallback provides the
+                // *(void *pSystemData) Call() function version however it is 
+                // empty
+                if(m_sceneMethod != NULL && m_pClassInstance != NULL) {
+                    if(!pSystemData) {
+                        return (m_pClassInstance->*m_sceneMethod)(NULL, NULL);
+                    } else {
+                        // #FIXME - when using void* parameter the most probable
+                        // scenario is that it's event structure of some kind
+                        CSceneNode* pNode = static_cast<CSceneNode *>(pSystemData);
+                        //if(pWidget->isManaged())
+                        return (m_pClassInstance->*m_sceneMethod)(pNode, NULL);
+                    }
+
+                }
+                return FG_FALSE;
+            }
+            /**
+             * 
+             * @param pWidget
+             * @return 
+             */
+            virtual fgBool Call(CSceneNode* pNodeA) {
+                if(m_sceneMethod != NULL && m_pClassInstance != NULL)
+                    return (m_pClassInstance->*m_sceneMethod)(pNodeA, NULL);
+                return FG_FALSE;
+            }
+            /**
+             * 
+             * @param pGuiMain
+             * @param pWidget
+             * @return 
+             */
+            virtual fgBool Call(CSceneNode* pNodeA, CSceneNode* pNodeB) {
+                if(m_sceneMethod != NULL && m_pClassInstance != NULL)
+                    return (m_pClassInstance->*m_sceneMethod)(pNodeA, pNodeB);
+                return FG_FALSE;
+            }
+            /**
+             * 
+             * @param pClassInstance
+             * @param pGuiMain
+             * @param pWidget
+             * @return 
+             */
+            fgBool Call(TClass *pClassInstance, CSceneNode* pNodeA = NULL, CSceneNode* pNodeB = NULL) {
+                if(m_sceneMethod != NULL && pClassInstance != NULL)
+                    return (pClassInstance->*m_sceneMethod)(pNodeA, pNodeB);
+                return FG_FALSE;
+            }
+            /**
+             * 
+             * @return 
+             */
+            inline fgSceneMethod& getSceneMethod(void) {
+                return m_sceneMethod;
+            }
+            /**
+             * 
+             * @return 
+             */
+            inline fgSceneMethod const& getSceneMethod(void) const {
+                return m_sceneMethod;
+            }
+
+        private:
+            /// 
+            TClass* m_pClassInstance;
+            ///
+            fgSceneMethod m_sceneMethod;
+        };
+
+        /**
+         * 
+         */
+        class CSceneFunctionCallback : public CSceneCallback {
+        public:
+            ///
+            typedef CSceneCallback base_type;
+            ///
+            typedef CSceneFunctionCallback self_type;
+            ///
+            typedef CSceneFunctionCallback type;
+            ///
+            typedef fgBool(*fgSceneFunction)(CSceneNode* pNodeA, CSceneNode* pNodeB);
+            ///
+            typedef fgSceneFunction callback_type;
+            /**
+             * 
+             * @param function
+             */
+            CSceneFunctionCallback(fgSceneFunction pFunction) :
+            CSceneCallback() {
+                m_sceneFunction = pFunction;
+            }
+            
+            /**
+             * 
+             */
+            virtual ~CSceneFunctionCallback() {
+                m_sceneFunction = NULL;
+            }
+            /**
+             * 
+             * @param function
+             */
+            void setSceneFunction(fgSceneFunction pFunction) {
+                m_sceneFunction = pFunction;
+            }
+            /**
+             * 
+             * @return 
+             */
+            virtual fgBool Call(void) {
+                if(m_sceneFunction != NULL)
+                    return m_sceneFunction(NULL, NULL);
+                return FG_FALSE;
+            }
+            /**
+             * 
+             * @param argv
+             * @return 
+             */
+            virtual fgBool Call(fg::event::CArgumentList* argv) {
+                if(m_sceneFunction != NULL) {
+                    // If argument list is given then maybe should check for existence
+                    // of STRUCT parameters ?
+                    //return (m_classInstance->*m_sceneMethod)(argv);
+                    return FG_FALSE;
+                }
+                return FG_FALSE;
+            }
+            /**
+             * 
+             * @param pSystemData
+             * @return 
+             */
+            virtual fgBool Call(void* pSystemData) {
+                // This is for compatibility - fgFunctionCallback provides the
+                // *(void *pSystemData) Call() function version however it is 
+                // empty
+                if(m_sceneFunction != NULL) {
+                    if(!pSystemData) {
+                        return m_sceneFunction(NULL, NULL);
+                    } else {
+                        CSceneNode* pNode = static_cast<CSceneNode *>(pSystemData);
+                        //if(pWidget->isManaged())
+                        return m_sceneFunction(pNode, NULL);
+                    }
+
+                }
+                return FG_FALSE;
+            }
+            /**
+             * 
+             * @param pWidget
+             * @return 
+             */
+            virtual fgBool Call(CSceneNode* pNodeA) {
+                if(m_sceneFunction != NULL)
+                    return m_sceneFunction(pNodeA, NULL);
+                return FG_FALSE;
+            }
+            /**
+             * 
+             * @param pGuiMain
+             * @param pWidget
+             * @return 
+             */
+            virtual fgBool Call(CSceneNode* pNodeA, CSceneNode* pNodeB) {
+                if(m_sceneFunction != NULL)
+                    return m_sceneFunction(pNodeA, pNodeB);
+                return FG_FALSE;
+            }
+            /**
+             * 
+             * @return 
+             */
+            inline fgSceneFunction& getSceneFunction(void) {
+                return m_sceneFunction;
+            }
+            /**
+             * 
+             * @return 
+             */
+            inline fgSceneFunction const& getSceneFunction(void) const {
+                return m_sceneFunction;
+            }
+
+        private:
+            ///
+            fgSceneFunction m_sceneFunction;
+        };
+    }
+}
+
+    #undef FG_INC_GFX_SCENE_CALLBACK_BLOCK
+#endif	/* FG_INC_GFX_SCENE_CALLBACK */
---
+++ src/GFX/Scene/fgGFXSceneEvent.h
@@ -0,0 +1,92 @@
+/*******************************************************************************
+ * Copyright (C) Radoslaw Gniazdowski <contact@flexigame.com>.
+ * All rights reserved.
+ * 
+ * This file is part of FlexiGame: Flexible Game Engine
+ * 
+ * FlexiGame source code and any related files can not be copied, modified 
+ * and/or distributed without the express or written consent from the author.
+ ******************************************************************************/
+/* 
+ * File:   fgGFXSceneEvent.h
+ * Author: vigilant
+ *
+ * Created on February 19, 2015, 12:26 AM
+ */
+
+#ifndef FG_INC_GFX_SCENE_EVENT
+    #define FG_INC_GFX_SCENE_EVENT
+    #define FG_INC_GFX_SCENE_EVENT_BLOCK
+
+    #include "Event/fgEventDefinitions.h"
+
+namespace fg {
+
+    namespace gfx {
+        class CSceneNode;
+        class CSceneNodeTrigger;
+    }
+
+    namespace event {
+
+        const EventType SCENE_NODE_COLLISION = LAST_STANDARD_EVENT_CODE + 100;
+        const EventType SCENE_NODE_TRIGGER_FIRED = LAST_STANDARD_EVENT_CODE + 101;
+        const EventType SCENE_NODE_INSERTED = LAST_STANDARD_EVENT_CODE + 102;
+        const EventType SCENE_NODE_REMOVED = LAST_STANDARD_EVENT_CODE + 103;
+        const EventType SCENE_NODE_DESTROYED = LAST_STANDARD_EVENT_CODE + 104;
+
+        const EventType SCENE_CLEARED = LAST_STANDARD_EVENT_CODE + 105;
+
+        const EventType SCENE_DUMMY = LAST_STANDARD_EVENT_CODE + 106;
+
+        const EventType LAST_SCENE_EVENT_CODE = LAST_STANDARD_EVENT_CODE + 106;
+
+        /*
+         *
+         */
+        struct SSceneNode : SEventBase {
+            gfx::CSceneNode* pNodeA;
+            gfx::CSceneNode* pNodeB;
+        };
+
+        /*
+         *
+         */
+        struct SSceneNodeCollision : SSceneNode {
+            //gfx::CollisionType collisionType;
+            //Vector3f center;
+        };
+
+        /**
+         *
+         */
+        struct SSceneNodeTrigger : SEventBase {
+
+            union {
+                gfx::CSceneNodeTrigger* pNodeTrigger;
+                gfx::CSceneNodeTrigger* pNodeA;
+            };
+            gfx::CSceneNode* pNodeB;
+        };
+
+        /**
+         *
+         */
+        struct SSceneEvent {
+
+            union {
+                EventType code;
+
+                SSceneNode node;
+                SSceneNodeCollision collision;
+                SSceneNodeTrigger trigger;
+
+                SReserved reserved;
+            };
+        };
+
+    }
+}
+
+    #undef FG_INC_GFX_SCENE_EVENT_BLOCK
+#endif	/* FG_INC_GFX_SCENE_EVENT */
