28ff217352042be4345ef08fa591cf3a011e99c1
Radosław Gniazdowski <7131876+rgniazdowski@users.noreply.github.com>
2015-04-20 14:33:43 +0200

    [GFX] SPlaneT refactor, fixes, additional functions, enums

--- src/GFX/fgGfxPlane.h
+++ src/GFX/fgGfxPlane.h
@@ -18,6 +18,7 @@
     #define FG_INC_GFX_PLANE
     #define FG_INC_GFX_PLANE_BLOCK
 
+    #include "fgBool.h"
     #include "fgGfxVertex.h"
     #include "Math/fgMathLib.h"
 
@@ -40,14 +41,30 @@ namespace fg {
 
             enum Halfspace {
                 NEGATIVE = -1,
-                ON_PLANE = 0,
+                NEUTRAL = 0,
                 POSITIVE = 1
+            };
+
+            enum Axis {
+                X,
+                Y,
+                Z,
+                ANY_X,
+                ANY_Y,
+                ANY_Z,
+                ANY
             };
 
             /// Normal vector
             vector_type n;
             /// 
             value_type d;
+            ///
+            Axis axis;
+            /**
+             * 
+             */
+            SPlaneT() : n(0, 0, 0), d(0), axis(Axis::ANY) { }
             /**
              * 
              * @param v1
@@ -59,46 +76,19 @@ namespace fg {
             }
             /**
              * 
-             * @param other
+             * @param normal
+             * @param point
              */
-            SPlaneT(const self_type& other) {
-                this->n = other.n;
-                this->d = other.d;
-            }
-            /**
-             * 
-             */
-            SPlaneT() : n(0), d(0) { }
-            /**
-             * 
-             */
-            ~SPlaneT() { }
-
-        public:
-            /**
-             * 
-             * @param v1
-             * @param v2
-             * @param v3
-             */
-            void set(const vector_type &v1, const vector_type &v2, const vector_type &v3) {
-                vector_type aux1, aux2;
-
-                aux1 = v1 - v2;
-                aux2 = v3 - v2;
-
-                this->n = aux2 * aux1;
-                this->n = fg::math::normalize(this->n);
-                this->d = -fg::math::dot(this->n, v2);
+            SPlaneT(const vector_type &normal, const vector_type &point) {
+                set(normal, point);
             }
             /**
              * 
              * @param normal
-             * @param point
+             * @param _d
              */
-            void set(const vector_type &normal, const vector_type &point) {
-                this->n = fg::math::normalize(normal);
-                this->d = -fg::math::dot(this->n, point);
+            SPlaneT(const vector_type &normal, value_type _d) {
+                set(normal, _d);
             }
             /**
              * 
@@ -107,41 +97,231 @@ namespace fg {
              * @param _c
              * @param _d
              */
-            void set(const float _a, const float _b, const float _c, const float _d) {
+            SPlaneT(value_type _a, value_type _b, value_type _c, value_type _d) {
+                set(_a, _b, _c, _d);
+            }
+            /**
+             * 
+             * @param other
+             */
+            SPlaneT(const self_type& other) {
+                this->n = other.n;
+                this->d = other.d;
+                this->axis = other.axis;
+            }
+            /**
+             * 
+             */
+            virtual ~SPlaneT() { }
+
+        public:
+            /**
+             * 
+             * @param v1
+             * @param v2
+             * @param v3
+             */
+            void set(const vector_type &v1,
+                     const vector_type &v2,
+                     const vector_type &v3) {
+                vector_type aux1 = v1 - v2;
+                vector_type aux2 = v3 - v2;
+
+                //this->n = aux2 * aux1;
+                this->n = math::cross(aux2, aux1);
+                this->n = math::normalize(this->n);
+                this->d = /*-*/math::dot(this->n, v2);
+                determineAxis();
+            }
+            /**
+             * 
+             * @param normal
+             * @param point
+             */
+            void set(const vector_type &normal, const vector_type &point) {
+                this->n = math::normalize(normal);
+                this->d = /*-*/math::dot(this->n, point);
+                determineAxis();
+            }
+            /**
+             * 
+             * @param normal
+             * @param _d
+             */
+            void set(const vector_type &normal, value_type _d) {
+                this->n = math::normalize(normal);
+                this->d = _d;
+                determineAxis();
+            }
+            /**
+             * 
+             * @param _a
+             * @param _b
+             * @param _c
+             * @param _d
+             */
+            void set(value_type _a, value_type _b, value_type _c, value_type _d) {
                 // set the normal vector
                 this->n = vector_type(_a, _b, _c);
                 this->d = _d;
-                this->normalize();
+                normalize();
+                //determineAxis();
             }
             /**
              * 
              */
             void normalize(void) {
-                float l = fg::math::length(this->n);
+                value_type l = math::length(this->n);
                 this->n.x = this->n.x / l;
                 this->n.y = this->n.y / l;
                 this->n.z = this->n.z / l;
                 this->d = this->d / l;
+                determineAxis();
             }
             /**
              * 
-             * @param _p
+             * @param point
              * @return 
              */
-            float distance(const vector_type &point) {
-                return (d + fg::math::dot(n, point));
+            value_type distance(const vector_type& point) {
+                // (math::dot(point, n) - d);
+                return (value_type)(this->d + math::dot(this->n, point));
                 //return (d + normal.innerProduct(p));
             }
             /**
              * 
-             * @param _p
+             * @param a
+             * @param b
+             * @param c
+             * @return
+             */
+            value_type distance(value_type a, value_type b, value_type c) {
+                // (math::dot(Vector3f(a, b, c), n) - d);
+                return (value_type)(this->d + math::dot(this->n, vector_type(a, b, c)));
+                //return (d + normal.innerProduct(p));
+            }
+            /**
+             * 
+             * @param point
              * @return 
              */
-            Halfspace classify(const vector_type &point) {
-                float _d = distance(point);
-                if(_d < 0) return NEGATIVE;
-                if(_d > 0) return POSITIVE;
-                return ON_PLANE;
+            Halfspace classify(const vector_type& point) {
+                value_type _d = distance(point);
+                if(_d < (value_type)0) return NEGATIVE;
+                if(_d > (value_type)0) return POSITIVE;
+                return NEUTRAL;
+            }
+            /**
+             *
+             * @return
+             */
+            Axis determineAxis(void) {
+                axis = Axis::ANY;
+                if(math::isZero(math::abs(n.x) - (value_type)1.0)) {
+                    axis = Axis::X;
+                } else if(math::isZero(math::abs(n.y) - (value_type)1.0)) {
+                    axis = Axis::Y;
+                } else if(math::isZero(math::abs(n.z) - (value_type)1.0)) {
+                    axis = Axis::Z;
+                }
+                return axis;
+            }
+            /**
+             * 
+             * @return
+             */
+            inline char getApproxAlignment(void) {
+                if(math::abs(n.x) > math::abs(n.y) && math::abs(n.x) > math::abs(n.z)) {
+                    return 'x';
+                } else if(math::abs(n.y) > math::abs(n.x) && math::abs(n.y) > math::abs(n.z)) {
+                    return 'y';
+                }
+                return 'z';
+            }
+            /**
+             *
+             * @param point
+             * @return
+             */
+            inline value_type fastDistance(const vector_type& point) {
+                switch(this->axis) {
+                    case Axis::X:
+                        if(n.x < 0) {
+                            return -(point.x - (-this->d));
+                        }
+                        return (point.x - this->d);
+                        break;
+                    case Axis::Y:
+                        if(n.y < 0) {
+                            return -(point.y - (-this->d));
+                        }
+                        return (point.y - this->d);
+                        break;
+                    case Axis::Z:
+                        if(n.z < 0) {
+                            return -(point.z - (-this->d));
+                        }
+                        return (point.z - this->d);
+                        break;
+                }
+                return (value_type)(math::dot(point, this->n) - this->d);
+            }
+            /**
+             *
+             */
+            inline void reverse(void) {
+                this->n *= -1.0f;
+                this->d = -this->d;
+            }
+            /**
+             * 
+             * @param vpos
+             * @param vdir
+             * @param ip
+             * @return 
+             */
+            fgBool rayIntersect(const vector_type& vpos,
+                                const vector_type& vdir,
+                                vector_type& ip) {
+                value_type dp = math::dot(this->n, vdir);
+                if(dp == 0.0) {
+                    return FG_FALSE;
+                }
+                ip = vpos - ((vdir * distance(vpos)) / dp);
+                return FG_TRUE;
+            }
+            /**
+             *
+             * @param a
+             * @param b
+             * @param v
+             * @return
+             */
+            fgBool getIP(const self_type &a,
+                         const self_type &b,
+                         vector_type &v) {
+                vector_type cp = math::cross(a.n, b.n);
+                value_type denom = math::dot(this->n, cp);
+
+                if(math::abs(denom) < (value_type)1e-5) {
+                    return FG_FALSE;
+                }
+                vector_type va = math::cross(a.n, b.n) * - this->d;
+                vector_type vb = math::cross(b.n, this->n) * a.d;
+                vector_type vc = math::cross(this->n, a.n) * b.d;
+                v = (va - vb - vc) / denom;
+                return FG_TRUE;
+            }
+            /**
+             * 
+             * @param p
+             * @return
+             */
+            fgBool operator ==(const self_type& p) const {
+                if(math::isZero(p.d - this->d)) {
+                    return math::isZero(math::dot(this->n, p.n) - (value_type)1.0);
+                }
+                return FG_FALSE;
             }
 
         };
@@ -158,11 +338,5 @@ namespace fg {
     }
 }
 
-    #include <cstdio>
-inline void dumpPlane(const fg::gfx::Planef & plane, const char *title) {
-    printf("%s = { %.2f %.2f %.2f d=%.2f}\n", (title ? title : "X"), plane.n.x, plane.n.y, plane.n.z, plane.d);
-}
-
     #undef FG_INC_GFX_PLANE_BLOCK
 #endif	/* FG_INC_GFX_PLANE */
-
